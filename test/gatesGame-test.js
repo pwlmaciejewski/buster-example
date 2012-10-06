94ae647c5ce4461d065e4409ef8416cdc66b1ece
buzun <pawel.maciejewski@allegro.pl>
2012-10-06 21:16:22 +0200

    Test for get random gate except

--- test/gatesGame-test.js
+++ test/gatesGame-test.js
@@ -1,37 +1,48 @@
 var testCase = buster.testCase('Game test case', {
   setUp: function () {
+    var that = this;
+    this.bareGame = Object.create(GatesGame);
     this.game = Object.create(GatesGame).initialize();
+    this.nTimes = function (method) {
+      return function () {
+        var res = [];
+        var args = Array.prototype.slice.call(arguments);
+        var num = args.shift();
+        for (var i = 0; i < num; i += 1) {
+          res.push(that.game[method].apply(that.game, args));
+        }
+        return res;
+      };
+    };
   },
 
   'test reset': function () {
-    var game = Object.create(GatesGame);
-    game.reset();
-    assert.equals(game.getPlayerGate(), -1);
-    assert.equals(game.getAwardGate(), -1);
-    assert.equals(game.getRejectedGates(), []);
+    this.bareGame.reset();
+    assert.equals(this.bareGame.getPlayerGate(), -1);
+    assert.equals(this.bareGame.getAwardGate(), -1);
+    assert.equals(this.bareGame.getRejectedGates(), []);
   },
 
   'test setOptions': {
     setUp: function () {
-      this.game = Object.create(GatesGame);
-      this.game._setOptions({
+      this.bareGame._setOptions({
         gatesCount: 5,
         decisionStrategy: 'dont change gate'
       });
     },
 
     'gated count': function () {
-      assert.equals(this.game.getGatesCount(), 5);      
+      assert.equals(this.bareGame.getGatesCount(), 5);      
     },
 
     'decision strategy': function () {
-      assert.equals(this.game.getDecisionStrategy(), 'dont change gate');
+      assert.equals(this.bareGame.getDecisionStrategy(), 'dont change gate');
     }
   },
 
   'test initialization': {
     'returned type': function () {
-      assert.equals(typeof Object.create(GatesGame).initialize(), 'object');
+      assert.equals(typeof this.game, 'object');
     }
   },
 
@@ -69,26 +80,18 @@ var testCase = buster.testCase('Game test case', {
 
   'test getRandomGate': {
     setUp: function () {
-      var that = this;
-
-      this.getRandomGateManyTimes = function (num) {
-        var res = [];
-        for (var i = 0; i < num; i += 1) {
-          res.push(that.game.getRandomGate());
-        }
-        return res;
-      };
+      this.getRandomGateNTimes = this.nTimes('getRandomGate');
     },
 
     'test lower bound': function () {
-      var gates = this.getRandomGateManyTimes(100);
+      var gates = this.getRandomGateNTimes(100);
       assert.equals(gates.filter(function (gate) {
         return gate < 0;        
       }).length, 0);
     },
 
     'test upper bound': function () {
-      var gates = this.getRandomGateManyTimes(100);
+      var gates = this.getRandomGateNTimes(100);
       var that = this.game;
       assert.equals(gates.filter(function (gate) {
         return gate >= that.getGatesCount();        
@@ -97,7 +100,7 @@ var testCase = buster.testCase('Game test case', {
 
     'test rejection': function () {
       this.game.rejectGate(0);
-      var gates = this.getRandomGateManyTimes(100);
+      var gates = this.getRandomGateNTimes(100);
       var that = this.game;
       assert.equals(gates.filter(function (gate) {
         return gate === 0;        
@@ -105,9 +108,25 @@ var testCase = buster.testCase('Game test case', {
     }
   },
 
-  decision: {
-    '// change': function () {
-      var game = Object.create(GatesGame).initialize();
+  'get random gate except': {
+    setUp: function () {
+      this.getRandomGateExceptNTimes = this.nTimes('getRandomGateExcept');
+    },
+
+    'test': function  () {      
+      assert.equals(this.getRandomGateExceptNTimes(100, [1, 2]).filter(function(gate) {
+        return gate === 1 || gate === 2;
+      }).length, 0);
+    }
+  },
+
+  '// decision': {
+    'change gate': function () {
+      this.bareGame.initialize({
+        decisionStrategy: 'change gate'
+      });
+      this.bareGame.setPlayerGate(1);
+      refute.equals(this.bareGame.makeDecision(), 1);
     }
   }
 });
